ed01cb0ea904fa77ffe622d3739e3b4c144a6479
didim99 <didim@eclabs.ru>
2021-03-24 19:31:47 +0300

    + Added some additional statistics  ! Minor code cleanup

--- app/src/main/java/ru/didim99/batterymonitor/ui/MainActivity.java
+++ app/src/main/java/ru/didim99/batterymonitor/ui/MainActivity.java
@@ -1,5 +1,6 @@
 package ru.didim99.batterymonitor.ui;
 
+import android.content.res.Resources;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -21,15 +22,23 @@ public class MainActivity extends AppCompatActivity {
     TextView tvBatteryLevel = findViewById(R.id.tvBatteryLevel);
     TextView tvLifetime = findViewById(R.id.tvLifetime);
     TextView tvUptime = findViewById(R.id.tvUptime);
+    TextView tvLastDCharge = findViewById(R.id.tvLastDCharge);
+    TextView tvLastDUsage = findViewById(R.id.tvLastDUsage);
 
+    Resources res = getResources();
     BatteryState state = BatteryState.load(this);
     tvLifetimeTitle.setText(state.isCharging() ?
       R.string.stat_chargeTime : R.string.stat_lifeTime);
     tvBatteryLevel.setText(getString(
       R.string.percentStr, state.getPercent()));
-    tvLifetime.setText(TimeUtils.getDetailedTimeString(
-      getResources(), state.getLifeTime()));
-    tvUptime.setText(TimeUtils.getDetailedTimeString(
-      getResources(), SystemClock.elapsedRealtime() / 1000));
+    tvLifetime.setText(describeTime(res, state.getLifeTime()));
+    tvUptime.setText(describeTime(res, SystemClock.elapsedRealtime() / 1000));
+    tvLastDCharge.setText(describeTime(res, state.getLastDCharge()));
+    tvLastDUsage.setText(describeTime(res, state.getLastDUsage()));
+  }
+
+  private String describeTime(Resources res, long time) {
+    if (time == 0) return getString(R.string.no_data);
+    else return TimeUtils.getDetailedTimeString(res, time);
   }
 }
--- app/src/main/java/ru/didim99/batterymonitor/utils/BatteryState.java
+++ app/src/main/java/ru/didim99/batterymonitor/utils/BatteryState.java
@@ -20,14 +20,19 @@ public class BatteryState {
   private static final String KEY_LAST_STATE = "battery.lastState";
   private static final String KEY_LAST_TIME = "battery.lastTime";
   private static final String KEY_LAST_TIME_FULL = "battery.lastTime.full";
+  private static final String KEY_LAST_DURATION_CHARGE = "stat.lastDuration.charge";
+  private static final String KEY_LAST_DURATION_USAGE = "stat.lastDuration.usage";
   private static final int DEFAULT_LAST_PERCENT = -1;
   private static final boolean DEFAULT_LAST_STATE = false;
   private static final long DEFAULT_LAST_TIME = 0;
   private static final long DEFAULT_LAST_TIME_FULL = 0;
+  private static final long DEFAULT_LAST_DURATION_CHARGE = 0;
+  private static final long DEFAULT_LAST_DURATION_USAGE = 0;
 
   private boolean isCharging, isLow;
   private int percent, lastPercent;
   private long lastTime, lastTimeFull;
+  private long lastDCharge, lastDUsage;
   private boolean prevState;
 
   private BatteryState(Context context, Intent batteryIntent) {
@@ -45,12 +50,29 @@ public class BatteryState {
     prevState = settings.getBoolean(KEY_LAST_STATE, DEFAULT_LAST_STATE);
     lastTime = settings.getLong(KEY_LAST_TIME, DEFAULT_LAST_TIME);
     lastTimeFull = settings.getLong(KEY_LAST_TIME_FULL, DEFAULT_LAST_TIME_FULL);
+    lastDCharge = settings.getLong(KEY_LAST_DURATION_CHARGE, DEFAULT_LAST_DURATION_CHARGE);
+    lastDUsage = settings.getLong(KEY_LAST_DURATION_USAGE, DEFAULT_LAST_DURATION_USAGE);
   }
 
   public void update(Context context) {
     if (isChargedNow()) updateTimeFull(context);
     if (needUpdatePercent()) updatePercent(context);
-    if (needUpdateTime()) updateTime(context);
+    if (needUpdateTime()) {
+      updateDuration(context);
+      updateTime(context);
+    }
+  }
+
+  private void updateDuration(Context context) {
+    long now = System.currentTimeMillis();
+    if (isCharging)
+      lastDUsage = now - lastTime;
+    else
+      lastDCharge = now - lastTime;
+
+    PreferenceManager.getDefaultSharedPreferences(context).edit()
+      .putLong(KEY_LAST_DURATION_CHARGE, lastDCharge)
+      .putLong(KEY_LAST_DURATION_USAGE, lastDUsage).apply();
   }
 
   private void updateTime(Context context) {
@@ -86,7 +108,7 @@ public class BatteryState {
       return true;
     if (isCharging && isFull())
       return false;
-    return prevState != isCharging
+    return isStateChanged()
       || isCharging && lastPercent > percent
       || !isCharging && lastPercent < percent;
   }
@@ -109,6 +131,10 @@ public class BatteryState {
     return isLow;
   }
 
+  private boolean isStateChanged() {
+    return prevState != isCharging;
+  }
+
   public int getPercent() {
     return percent;
   }
@@ -117,6 +143,14 @@ public class BatteryState {
     long now = (isCharging && isFull()) ?
       lastTimeFull : System.currentTimeMillis();
     return (now - lastTime) / 1000;
+  }
+
+  public long getLastDCharge() {
+    return lastDCharge;
+  }
+
+  public long getLastDUsage() {
+    return lastDUsage;
   }
 
   public static BatteryState load(Context context) {
